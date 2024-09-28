5515562b554e8fbdd6dba03a3f4c209a3b628075
fchristian1 <fchristian1@gmx.net>
2024-09-28 22:17:07 +0200

    20240928-221707

--- frontend/sudoku/src/App.jsx
+++ frontend/sudoku/src/App.jsx
@@ -4,8 +4,16 @@ function App() {
     return (
         <>
             <h1 className="self-center text-4xl text-orange-500 underline">
-                Sudoku
+                Sudoku v0.0.1
             </h1>
+            <a
+                className="self-center underline text-blue-500"
+                href="https://github.com/fchristian1/sudoku-react"
+                target="_blank"
+            >
+                myGitHub Link
+            </a>
+            <br />
             <SudokuView />
         </>
     );

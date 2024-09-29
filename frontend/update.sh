ebc13fe061ad8cb821f7ac966ab958ba265d61e1
fchristian1 <fchristian1@gmx.net>
2024-09-30 01:21:04 +0200

    chore: font fix

--- frontend/update.sh
+++ frontend/update.sh
@@ -2,4 +2,4 @@
 git pull && docker stop sudoku
 docker rm sudoku
 docker build . -t sudoku:latest -t sudoku:0.0.2
-docker run -d -p 9001:80 --name sudoku sudoku
+docker run -d -p 9001:80 --name sudoku sudoku:latest
